66edccc43350468975894f6a94db1a456b54e01e
Pravesh981 <121166656+Pravesh981@users.noreply.github.com>
2024-02-29 01:22:38 +0530

    Update EmployeeMain.java

--- Employee Loan Eligibility/src/employee/com/EmployeeMain.java
+++ Employee Loan Eligibility/src/employee/com/EmployeeMain.java
@@ -4,8 +4,11 @@ public class EmployeeMain {
 	  public static void main(String[] args) {
 	        PermanentEmployee permanentEmployee = new PermanentEmployee(123, "Saksham", 1123234);
 	        TemporaryEmployee temporaryEmployee = new TemporaryEmployee(112, "dubey",6,100);
-
-	        Loan loan = new Loan();
+		  
+                  permanentEmployee.calculateSalary(); 
+	          temporaryEmployee.calculateSalary();
+	      
+		  Loan loan = new Loan();
 	        double permanentEmployeeLoan = loan.calculateLoanAmount(permanentEmployee);
 	        double temporaryEmployeeLoan = loan.calculateLoanAmount(temporaryEmployee);
 
